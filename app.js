180500b9c2113f5b814f55fb150f090abb98f840
Avi Davis Mac <oodavis.avi@gmail.com>
2018-07-08 20:56:28 -0700

    Modified Database Config.

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const COOKIEPARSER = require('cookie-parser');
 const LOGGER = require('morgan');
 const BODYPARSER = require('body-parser');
 const MONGOOSE = require("mongoose");
-require("dotenv")
-
+require('dotenv').config()
 
 const APP = EXPRESS();
 
@@ -27,11 +26,21 @@ APP.use(BODYPARSER.urlencoded({ extended: false }));
 APP.use(EXPRESS.static(PATH.join(__dirname, 'public')));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsappd";
 
-//MONGOOSE.connect("mongodb://localhost/newsappd");
-
-MONGOOSE.connect(MONGODB_URI);
+ 
+MONGOOSE.connect(MONGODB_URI, options);
+var conn = MONGOOSE.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
 
 
 // routes ======================================================================
--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -4,7 +4,7 @@ const AXIOS = require("axios");
 const CHEERIO = require("cheerio");
 
 // Require all models
-const DB = require("./models");
+const DB = require("../models");
 
 module.exports = {
     fetch: function (req, res, next) {
@@ -22,7 +22,7 @@ module.exports = {
                 // Save the text of the h4-tag as "title"
                 var title = $(this).text();
                 var link = $(this).children("a").attr("href");
-                var date = $npm(this).siblings(".date").text();
+                var date = $(this).siblings(".date").text();
                 var excerpt = $(this).siblings(".teaser").text();
                 var image = $(this).siblings(".image").children("a").children("img").attr("src");
                 var added = false;
